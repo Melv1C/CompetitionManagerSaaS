26d38addd973e050711dae8be8388ae5605d2391
Melv1C <melvyn.claes@student.uclouvain.be>
2024-11-01 18:17:08 +0100

    refactor(users-api): routes folder

--- apps/users-api/src/index.ts
+++ apps/users-api/src/index.ts
@@ -1,54 +1,16 @@
 import express from "express";
 import 'dotenv/config';
-import { z } from 'zod';
-import { parseRequest } from '@competition-manager/utils';
-import { prisma } from "@competition-manager/prisma";
-import { User$ } from '@competition-manager/schemas';
+
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-const service_name = process.env.SERVICE_NAME || 'users';
-const prefix = `/api/${service_name}`;
+const PREFIX = process.env.PREFIX || '/api';
 
-const Body$ = User$.pick({ firebaseId: true, email: true });
-
-app.post(`${prefix}`, parseRequest('body', Body$), async (req, res) => {
-    const userBody = Body$.parse(req.body);
-    //create user in postgres
-    const user = await prisma.user.create({
-        data: {
-            ...userBody,
-            preferences: {
-                create: {
-                    theme: 'light',
-                    language: 'fr'
-                }
-            },
-            role: 'user'
-        }
-    });
-    res.send(user);
-});
-
-const Params$ = z.object({
-    firebaseId: z.string().min(1)
-});
-
-app.get(`${prefix}/:firebaseId`, parseRequest('params', Params$), async (req, res) => {
-    const { firebaseId } = Params$.parse(req.params);
-    const user = await prisma.user.findUnique({
-        where: {
-            firebaseId: firebaseId
-        },
-        include: {
-            preferences: true
-        }
-    });
-    res.send(user);
-});
+app.use(`${PREFIX}/users`, routes);
 
 app.listen(port, () => {
-    console.log(`${service_name}: app is running at port : ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
