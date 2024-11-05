3adf48a1935f3266d9add96df29bea90e9649968
Riwan <riwan.claes@student.uclouvain.be>
2024-11-05 12:59:38 +0100

    refactor: remove BeathleticsResult from shared package and add utils folder

--- apps/athletes-api/src/routes/getRecords.ts
+++ apps/athletes-api/src/routes/getRecords.ts
@@ -1,38 +1,11 @@
 import { Router } from 'express';
 import { z } from 'zod';
-import axios from 'axios';
 import 'dotenv/config';
 import { parseRequest, bestResult } from '@competition-manager/utils';
-import { BeathleticsResult, BeathleticsResult$ } from '@competition-manager/schemas';
+import { BeathleticsResult } from '../utils/BeathleticsResult';
+import { getResults } from '../utils/getResult';
 
 export const router = Router();
-
-const getResults = async (license: string, events: string[], maxYears: number) => {
-    const { data } = await axios.get(process.env.RECORDS_API + `/athlete/new/${license}`);
-    const results = data.results;
-    let records: BeathleticsResult[] = [];
-    for (let result of results) {
-        if (!result.result.discipline.eventType) continue;
-        let discipline = result.result.discipline.eventType.name_fr;
-        if (!events.includes(discipline)) continue;
-        let date = new Date(result.result.discipline.competition.startDate);
-        if (maxYears < new Date().getFullYear() - date.getFullYear()) continue;
-        if (result.result.newTrials && result.result.newTrials.length > 0) {
-            for (let j = 0; j < result.result.newTrials.length; j++) {
-                if (result.result.newTrials[j].homologationBest) {
-                    records.push(BeathleticsResult$.parse({
-                        discipline: discipline,
-                        date: date,
-                        perf: parseFloat(result.result.newTrials[j].rankingPerf),
-                        type: result.result.newTrials[j].perftype
-                    }));
-                }
-            }
-        }
-    }
-    return records;
-}
-
 
 const Param$ = z.object({
     license: z.string().min(1, {message: 'License must be at least 1 characters long'})
---
+++ apps/athletes-api/src/utils/getResult.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { BeathleticsResult, BeathleticsResult$ } from './BeathleticsResult';
+
+export const getResults = async (license: string, events: string[], maxYears: number) => {
+    const { data } = await axios.get(process.env.RECORDS_API + `/athlete/new/${license}`);
+    const results = data.results;
+    let records: BeathleticsResult[] = [];
+    for (let result of results) {
+        if (!result.result.discipline.eventType) continue;
+        let discipline = result.result.discipline.eventType.name_fr;
+        if (!events.includes(discipline)) continue;
+        let date = new Date(result.result.discipline.competition.startDate);
+        if (maxYears < new Date().getFullYear() - date.getFullYear()) continue;
+        if (result.result.newTrials && result.result.newTrials.length > 0) {
+            for (let j = 0; j < result.result.newTrials.length; j++) {
+                if (result.result.newTrials[j].homologationBest) {
+                    records.push(BeathleticsResult$.parse({
+                        discipline: discipline,
+                        date: date,
+                        perf: parseFloat(result.result.newTrials[j].rankingPerf),
+                        type: result.result.newTrials[j].perftype
+                    }));
+                }
+            }
+        }
+    }
+    return records;
+}
