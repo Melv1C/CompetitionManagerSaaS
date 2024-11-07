dea79ca5587cf653c893ffbf886ce431ad1dff9e
Melv1C <melvyn.claes@student.uclouvain.be>
2024-11-07 14:39:18 +0100

    feat(frontend): :lipstick: mui theme provider

--- apps/competition-manager-web/src/App.tsx
+++ apps/competition-manager-web/src/App.tsx
@@ -1,10 +1,25 @@
+import { createTheme, ThemeProvider, Typography } from "@mui/material"
+
+// must be extract in an other file
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#005ADF',
+    },
+    secondary: {
+      main: '#FF4141',
+    },
+  },
+})
 
 function App() {
   
   return (
-    <>
-      Hello world!
-    </>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1">
+        Hello World!
+      </Typography>
+    </ThemeProvider>
   )
 }
 
