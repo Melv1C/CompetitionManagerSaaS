90a1bf22fd991d8dc7567ae37f01ea9f077d0924
Riwan <riwan.claes@student.uclouvain.be>
2024-11-07 11:23:21 +0100

    refactor(users-api): Remove getUser

--- apps/users-api/src/routes/index.ts
+++ apps/users-api/src/routes/index.ts
@@ -1,4 +1,3 @@
-import { router as getUserRouter } from './getUser';
 import { router as registerUserRouter} from './registerUser';
 import { router as loginUserRouter } from './loginUser';
 import { router as refreshTokenRouter } from './refreshToken';
@@ -8,6 +7,5 @@ export default [
     registerUserRouter,
     loginUserRouter,
     refreshTokenRouter,
-    logoutUserRouter,
-    getUserRouter  // Need to be last because of the param route
+    logoutUserRouter
 ];
