b6664cfc4eb22997ad9ac5e1f9b02e552b477149
Riwan <riwan.claes@student.uclouvain.be>
2024-11-28 14:55:49 +0100

    feat: add rate limiter for login

--- apps/users-api/src/routes/loginUser.ts
+++ apps/users-api/src/routes/loginUser.ts
@@ -1,12 +1,23 @@
 import { Router } from 'express';
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
+import { rateLimit } from 'express-rate-limit'
 import { prisma } from '@competition-manager/prisma';
 import { parseRequest, generateAccessToken, generateRefreshToken } from '@competition-manager/utils';
 import { User$ } from '@competition-manager/schemas';
 import { UserToTokenData } from '../utils';
 
 export const router = Router();
+
+
+const loginLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	legacyHeaders: false, // Do not show limit, remaning, reset in respond headers 
+    message: 'Too many login request, please try again later.',
+    skipSuccessfulRequests: true,  // By default all response with code below 400 are considered successful
+    statusCode: 429
+})
 
 const Body$ = z.object({
     email: z.string({ message: 'Email must be a string' }).email({ message: 'Email must be a valid email' }),
@@ -15,6 +26,7 @@ const Body$ = z.object({
 
 router.post(
     '/login',
+    loginLimiter,
     parseRequest('body', Body$),
     async (req, res) => {
         const { email, password } = Body$.parse(req.body);
