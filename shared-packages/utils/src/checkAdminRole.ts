9658099885c3e97fd45be26e5f75f9f57c6817d7
Riwan <riwan.claes@student.uclouvain.be>
2024-12-10 11:32:45 +0100

    fix(Utils): fix import checkadmin role

--- shared-packages/utils/src/checkAdminRole.ts
+++ shared-packages/utils/src/checkAdminRole.ts
@@ -1,17 +1,17 @@
 import { Response } from 'express';
-import { AdminFromCompetition, AdminFromCompetiton$, Access } from '@competition-manager/schemas';
+import { BaseAdmin, BaseAdmin$, Access } from '@competition-manager/schemas';
 
 
 
-const isAdminAuthorized = (admin: AdminFromCompetition, levelRequire: Access) => {
+const isAdminAuthorized = (admin: BaseAdmin, levelRequire: Access) => {
     if (admin.access.includes('owner')) {
         return true;
     }
     return admin.access.includes(levelRequire);
 }
 
-export const checkAdminRole = async (levelRequire: Access, userId: number, admins: AdminFromCompetition[], res: Response) => {
-    const admin = AdminFromCompetiton$.parse(admins.find(admin => admin.userId === userId));
+export const checkAdminRole = async (levelRequire: Access, userId: number, admins: BaseAdmin[], res: Response) => {
+    const admin = BaseAdmin$.parse(admins.find(admin => admin.userId === userId));
     if (!admin) {
         res.status(401).send('Unauthorized');
         return false;
