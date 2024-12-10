d12ac751bed5e303d9c5268847b4d58fedae9bb0
Riwan <riwan.claes@student.uclouvain.be>
2024-12-10 10:05:21 +0100

    feat(competitions-api): delete compet

---
+++ apps/competitions-api/src/routes/competitions/deleteCompetition.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import { prisma } from '@competition-manager/prisma';
+import { z } from 'zod';
+import { parseRequest, checkRole, checkAdminRole, AuthenticatedRequest } from '@competition-manager/utils';
+import { AdminFromCompetiton$, Eid$ } from '@competition-manager/schemas';
+
+export const router = Router();
+
+const Params$ = z.object({
+    competitionEid: Eid$
+});
+
+router.delete(
+    '/:competitionEid',
+    parseRequest('params', Params$),
+    checkRole('admin'),
+    async (req: AuthenticatedRequest, res) => {
+        try{
+            const { competitionEid } = Params$.parse(req.params);
+            const competition = await prisma.competition.findUnique({
+                where: {
+                    eid: competitionEid
+                },
+                include: {
+                    admins: {
+                        select: {
+                            access: true,
+                            userId: true
+                        }
+                    },
+                }
+            });
+            if (!competition) {
+                res.status(404).send('Competition not found');
+                return;
+            }
+            if (!checkAdminRole('owner', req.user!.id, z.array(AdminFromCompetiton$).parse(competition.admins), res)) {
+                return;
+            }
+            await prisma.competition.delete({
+                where: {
+                    eid: competitionEid
+                }
+            });
+            res.send('Competition deleted');
+            
+        } catch (e) {
+            console.error(e);
+            res.status(500).send('Internal server error');
+        }
+    }
+);
