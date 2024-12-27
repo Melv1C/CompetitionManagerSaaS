bee9d5e66e457f62a36e01574f153f83525fee06
Melv1C <melvyn.claes@student.uclouvain.be>
2024-12-27 13:24:16 +0100

    fix(shared-packages): use native Enum in Competition$

--- shared-packages/schemas/src/Competition.ts
+++ shared-packages/schemas/src/Competition.ts
@@ -4,11 +4,18 @@ import { Admin$ } from './Admin';
 import { Club$ } from './Club';
 import { Option$, PaymentPlan$ } from './PaymentPlan';
 
-export const PAYMENT_METHOD = ['free', 'online', 'onPlace'] as const;
-export type PaymentMethod = typeof PAYMENT_METHOD[number];
 
-export const ONE_DAY_PERMISSION = ['foreing', 'all', 'bpm'] as const;
-export type OneDayPermission = typeof ONE_DAY_PERMISSION[number];
+export enum PAYMENT_METHOD {
+    FREE = 'free',
+    ONLINE = 'online',
+    ONPLACE = 'onPlace',
+}
+
+export enum ONE_DAY_PERMISSION {
+    FOREING = 'foreing',
+    ALL = 'all',
+    BPM = 'bpm',
+}
 
 export const Competition$ = z.object({
     id: z.number().positive(),
@@ -22,7 +29,7 @@ export const Competition$ = z.object({
     description: z.string().default(''),
     events: z.array(CompetitionEvent$).default([]),
     publish: z.boolean().default(false),
-    method: z.enum(PAYMENT_METHOD),
+    method: z.nativeEnum(PAYMENT_METHOD).default(PAYMENT_METHOD.FREE),
     paymentPlan: PaymentPlan$,
     options: z.array(Option$).default([]),
     startInscriptionDate: z.coerce.date().min(new Date()),
@@ -35,7 +42,7 @@ export const Competition$ = z.object({
     // Advanced settings
     closeDate: z.coerce.date().min(new Date()).optional(),
     freeClubs: z.array(Club$).default([]),
-    oneDayPermissions: z.array(z.enum(ONE_DAY_PERMISSION)).default([]),
+    oneDayPermissions: z.array(z.nativeEnum(ONE_DAY_PERMISSION)).default([]),
     oneDayBibStart: z.number().positive().max(9999).min(9900).default(9900),
         
 });
