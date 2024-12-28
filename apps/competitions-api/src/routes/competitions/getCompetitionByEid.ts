6d9c570eac464800b1f030a2d51c8d27fb1cbbdb
Riwan <riwan.claes@student.uclouvain.be>
2024-12-28 10:53:14 +0100

    feat(competitions-api): get compet by eid admin

--- apps/competitions-api/src/routes/competitions/getCompetitionByEid.ts
+++ apps/competitions-api/src/routes/competitions/getCompetitionByEid.ts
@@ -1,20 +1,55 @@
 import { Router } from 'express';
 import { prisma } from '@competition-manager/prisma';
-import { Key, parseRequest } from '@competition-manager/utils';
-import { Eid$ } from '@competition-manager/schemas';
+import { Key, parseRequest, checkRole, AuthenticatedRequest } from '@competition-manager/utils';
+import { Eid$, Role, AdminCompetition$ } from '@competition-manager/schemas';
 import { z } from 'zod';
 
 export const router = Router();
 
 const Params$ = z.object({
-    competitionEid: Eid$
+    competitionEid: Eid$,
+});
+
+const Query$ = z.object({
+    isAdmin: z.boolean().default(false),
 });
 
 router.get(
     '/:competitionEid',
     parseRequest(Key.Params, Params$),
-    async (req, res) => {
+    parseRequest(Key.Query, Query$),
+    checkRole(Role.ADMIN, Key.Query, 'isAdmin'),
+    async (req: AuthenticatedRequest, res) => {
         const { competitionEid } = Params$.parse(req.params);
+        if (req.query.isAdmin) {
+            const admin = await prisma.admin.findFirst({
+                where: {
+                    userId: req.user!.id,
+                    competition: {
+                        eid: competitionEid,
+                    },
+                },
+                select: {
+                    competition: {
+                        include: {
+                            events: true,
+                            paymentPlan: {
+                                include: {
+                                    includedOptions: true,
+                                },
+                            },
+                            options: true,
+                        },
+                    },
+                },
+            });
+            if (!admin) {
+                res.status(404).send('Competition not found');
+                return;
+            }
+            res.send(AdminCompetition$.parse(admin.competition));
+        }
+
         const competition = await prisma.competition.findUnique({
             where: {
                 eid: competitionEid,
