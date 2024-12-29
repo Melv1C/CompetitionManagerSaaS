f9b3f880406bc487ecdcfc58481c7612de648a58
Riwan <riwan.claes@student.uclouvain.be>
2024-12-29 20:15:18 +0100

    feat(categories-api): create a function to fill the DB

---
+++ apps/categories-api/src/fillDB.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+import { prisma } from '@competition-manager/prisma';
+import { CategoryWithoutId$ } from '@competition-manager/schemas';
+import catData from './categories.json';
+
+export const fillDB = async () => {
+    const categories = await prisma.category.findMany();
+    for (const cat of catData) {
+        try {
+            const category = CategoryWithoutId$.parse(cat);
+            const existingCategory = categories.find(c => c.name === category.name);
+            if (existingCategory) {
+                await prisma.category.update({
+                    where: { id: existingCategory.id },
+                    data: category
+                });
+                continue;
+            }
+            await prisma.category.create({
+                data: category
+            });
+        } catch (error) {
+            console.log('Error parsing category', cat);
+            console.error(error);
+            continue;
+        }
+    }
+    console.log('DB category filled');
+}
