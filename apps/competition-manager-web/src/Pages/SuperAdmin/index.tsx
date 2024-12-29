006fecdb2b92402aa2ce84ef8e1a19fd8df3ecdb
Melv1C <melvyn.claes@student.uclouvain.be>
2024-12-29 10:12:42 +0100

    feat(frontend): Sidenav and routes for the superadmin dashboard

--- apps/competition-manager-web/src/Pages/SuperAdmin/index.tsx
+++ apps/competition-manager-web/src/Pages/SuperAdmin/index.tsx
@@ -4,6 +4,9 @@ import { Box, Typography } from "@mui/material"
 import { SideNav } from "../../Components/SideNav";
 import { faReceipt } from "@fortawesome/free-solid-svg-icons";
 import { Route, Routes } from "react-router-dom";
+import { Offers } from "./Offers";
+import { CLOSED_SIDENAV_WIDTH, OPEN_SIDENAV_WIDTH } from "../../utils/constants";
+import { useState } from "react";
 
 const MenuItems = [
     { text: 'Offers', icon: faReceipt, link: '/superadmin/offers' },
@@ -11,17 +14,27 @@ const MenuItems = [
 
 export const SuperAdmin = () => {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <Box
             sx={{
-                display: 'flex'
+                display: 'flex',
+                width: '100%',
             }}
         >
-            <SideNav items={MenuItems} />
-            <Routes>
-                <Route path="/" element={<Typography variant="h4">SuperAdmin</Typography>} />
-                <Route path="/offers" element={<Typography variant="h4">Offers</Typography>} />
-            </Routes>
+            <SideNav items={MenuItems} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Box
+                sx={{
+                    width: `calc(100% - ${isMenuOpen ? OPEN_SIDENAV_WIDTH : CLOSED_SIDENAV_WIDTH})`,
+                    transition: 'width 0.3s',
+                }}
+            >
+                <Routes>
+                    <Route path="/" element={<Typography variant="h4">SuperAdmin</Typography>} />
+                    <Route path="/offers" element={<Offers />} />
+                </Routes>
+            </Box>
         </Box>
     )
 }
