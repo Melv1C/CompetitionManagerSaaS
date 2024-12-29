6397c18fb5ec26f63ea6c1d4a9364e98a9e614e8
Riwan <riwan.claes@student.uclouvain.be>
2024-12-29 12:06:25 +0100

    feat: add unconfirmedUser role

--- shared-packages/schemas/src/User.ts
+++ shared-packages/schemas/src/User.ts
@@ -3,6 +3,7 @@ import { UserPreferences$ } from './UserPreferences';
 import { Id$ } from './Base';
 
 export enum Role {
+    UNCONFIRMED_USER = 'unconfirmedUser',
     USER = 'user',
     ADMIN = 'admin',
     CLUB = 'club',
@@ -10,10 +11,11 @@ export enum Role {
 }
 
 export const RoleLevel = {
-    [Role.USER]: 0,
-    [Role.ADMIN]: 1,
-    [Role.CLUB]: 2,
-    [Role.SUPERADMIN]: 3,
+    [Role.UNCONFIRMED_USER]: 0,
+    [Role.USER]: 1,
+    [Role.ADMIN]: 2,
+    [Role.CLUB]: 3,
+    [Role.SUPERADMIN]: 4,
 }
 
 export const UserEmail$ = z.string().email();
@@ -22,9 +24,8 @@ export const UserPassword$ = z.string().min(8, 'Password must be at least 8 char
 export const User$ = z.object({
     id: Id$,
     email: UserEmail$,
-    role: z.nativeEnum(Role),
+    role: z.nativeEnum(Role).default(Role.UNCONFIRMED_USER),
     preferences: UserPreferences$.optional(),
     password: z.string()
 });
-
 export type User = z.infer<typeof User$>;
