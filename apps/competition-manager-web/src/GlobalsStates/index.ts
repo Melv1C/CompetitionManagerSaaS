a45aacae287413badc40b8e49104f260ed630c90
Melv1C <melvyn.claes@student.uclouvain.be>
2024-12-30 16:18:32 +0100

    feat(frontend): competition state

--- apps/competition-manager-web/src/GlobalsStates/index.ts
+++ apps/competition-manager-web/src/GlobalsStates/index.ts
@@ -1,4 +1,6 @@
-import { TokenData } from '@competition-manager/schemas';
+import { Competition, TokenData } from '@competition-manager/schemas';
 import { atom } from 'jotai';
 
 export const userTokenAtom = atom<TokenData | 'NOT_LOGGED' | null>(null);
+
+export const competitionAtom = atom<Competition | null>(null);
