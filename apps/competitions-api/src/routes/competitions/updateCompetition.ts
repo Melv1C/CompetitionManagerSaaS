23573e3ba2e8c307d05880606fc3864fb1dbe960
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-02 18:01:18 +0100

    fix(updateCompetition): correct role check logic for admin access

--- apps/competitions-api/src/routes/competitions/updateCompetition.ts
+++ apps/competitions-api/src/routes/competitions/updateCompetition.ts
@@ -33,7 +33,7 @@ router.put(
                 res.status(404).send('Competition not found');
                 return;
             }
-            if (req.user!.role == Role.SUPERADMIN || !checkAdminRole(Access.COMPETITIONS, req.user!.id, BaseAdmin$.array().parse(competition.admins), res)) return;
+            if (req.user!.role !== Role.SUPERADMIN && !checkAdminRole(Access.COMPETITIONS, req.user!.id, BaseAdmin$.array().parse(competition.admins), res)) return;
             try {
                 const updatedCompetition = await prisma.competition.update({
                     where: {
