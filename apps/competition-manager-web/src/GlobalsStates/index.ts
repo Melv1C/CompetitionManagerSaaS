106de8757be115fa853bdf7bbca585c4bdbb2b06
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-13 01:18:56 +0100

    feat(globals): add inscriptionsAtom to manage competition inscriptions state

--- apps/competition-manager-web/src/GlobalsStates/index.ts
+++ apps/competition-manager-web/src/GlobalsStates/index.ts
@@ -1,6 +1,7 @@
-import { Competition, TokenData } from '@competition-manager/schemas';
+import { Competition, Inscription, TokenData } from '@competition-manager/schemas';
 import { atom } from 'jotai';
 
 export const userTokenAtom = atom<TokenData | 'NOT_LOGGED' | null>(null);
 
 export const competitionAtom = atom<Competition | null>(null);
+export const inscriptionsAtom = atom<Inscription[] | null>(null);
