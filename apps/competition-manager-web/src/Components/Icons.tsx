c33a78c42213dec5e243d6e1822e9f9156550492
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-13 01:18:43 +0100

    feat(icons): refactor icon components to use BaseIcon and add Users icon

--- apps/competition-manager-web/src/Components/Icons.tsx
+++ apps/competition-manager-web/src/Components/Icons.tsx
@@ -1,31 +1,48 @@
 
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
-import { faEdit, faFloppyDisk, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faFloppyDisk, faPlus, faTrash, faUsers, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+
 
 type IconProps = Omit<FontAwesomeIconProps, 'icon'>
 
+type BaseIconProps = IconProps & {
+    icon: IconDefinition
+}
+
+export const BaseIcon: React.FC<BaseIconProps> = ({ icon, ...props }) => {
+    return (
+        <FontAwesomeIcon icon={icon} {...props} />
+    )
+}
+
 export const Add: React.FC<IconProps> = (props) => {
     return (
-      <FontAwesomeIcon icon={faPlus} {...props} />
+        <BaseIcon icon={faPlus} {...props} />
     )
 }
 
 export const Edit: React.FC<IconProps> = (props) => {
     return (
-      <FontAwesomeIcon icon={faEdit} {...props} />
+        <BaseIcon icon={faEdit} {...props} />
     )
 }
 
 export const Delete: React.FC<IconProps> = (props) => {
     return (
-      <FontAwesomeIcon icon={faTrash} {...props} />
+        <BaseIcon icon={faTrash} {...props} />
     )
 }
 
 export const Save: React.FC<IconProps> = (props) => {
-	return (
-	  <FontAwesomeIcon icon={faFloppyDisk} {...props} />
-	)
+    return (
+        <BaseIcon icon={faFloppyDisk} {...props} />
+    )
+}
+
+export const Users: React.FC<IconProps> = (props) => {
+    return (
+        <BaseIcon icon={faUsers} {...props} />
+    )
 }
 
 
