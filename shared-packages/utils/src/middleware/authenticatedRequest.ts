e2c561ed882c26edfe95a0b5006a79704e6fffcb
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-13 11:41:30 +0100

    feat(auth): update user type from UserFromToken to TokenData in authorization middleware

--- shared-packages/utils/src/middleware/authenticatedRequest.ts
+++ shared-packages/utils/src/middleware/authenticatedRequest.ts
@@ -1,14 +1,7 @@
 import { Request } from 'express';
-import { User$ } from '@competition-manager/schemas';
+import { TokenData, User$ } from '@competition-manager/schemas';
 import { z } from 'zod';
 
-const UserFromToken$ = User$.omit({
-    password: true,
-});
-
-export type UserFromToken = z.infer<typeof UserFromToken$>;
-
-
 export interface AuthenticatedRequest extends Request {
-    user?: UserFromToken;
+    user?: TokenData;
 }
