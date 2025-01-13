58959cde07362c7e93469bfddad315022b592af7
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-13 11:41:58 +0100

    feat(protected-route): add ProtectedRoute component for role-based access control

---
+++ apps/competition-manager-web/src/Components/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+import { useAtomValue } from "jotai";
+import { PropsWithChildren } from "react"
+import { userTokenAtom } from "../GlobalsStates";
+import { Role } from "@competition-manager/schemas";
+import { isAuthorized } from "@competition-manager/utils";
+import { Navigate } from "react-router-dom";
+
+
+type ProtectedRouteProps = PropsWithChildren<{
+    requiredRole: Role;
+    redirectPath: string;
+}>;
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ 
+    requiredRole,
+    redirectPath,
+    children 
+}) => {
+    const userToken = useAtomValue(userTokenAtom);
+
+    if (!userToken) throw new Error('UserToken is not defined');
+
+    if (userToken === 'NOT_LOGGED' || !isAuthorized(userToken, requiredRole)) {
+        return <Navigate to={redirectPath} />;
+    }
+
+    return <>{children}</>;
+
+}
+
+
