b1efa6bf056a03625d040bb066650f3d61e5137f
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-18 15:14:35 +0100

    refactor(App): clean up component structure and improve readability

--- apps/competition-manager-web/src/App.tsx
+++ apps/competition-manager-web/src/App.tsx
@@ -27,28 +27,27 @@ import { Role } from '@competition-manager/schemas';
 
 // must be extract in an other file
 const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#184c85',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#ff7043',
-      contrastText: '#ffffff',
-    },
-  },
-  components: {
-    MuiDrawer: {
-      styleOverrides: {
-        paper: {
-          backgroundColor: '#184c85',
-          color: '#ffffff',
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#184c85',
+            contrastText: '#ffffff',
         },
-      },
+        secondary: {
+            main: '#ff7043',
+            contrastText: '#ffffff',
+        },
     },
-
-  },
+    components: {
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    backgroundColor: '#184c85',
+                    color: '#ffffff',
+                },
+            },
+        },
+    },
 })
 
 // Create a client
@@ -56,61 +55,65 @@ const queryClient = new QueryClient()
 
 function App() {
 
-  const navItems = [
-    { label: 'Home', href: '/', icon: faHome },
-    { label: 'Competitions', href: '/competitions', icon: faCalendarDays },
-    { label: 'Results', href: '/results', icon: faRankingStar },
-  ]
+    const navItems = [
+        { label: 'Home', href: '/', icon: faHome },
+        { label: 'Competitions', href: '/competitions', icon: faCalendarDays },
+        { label: 'Results', href: '/results', icon: faRankingStar },
+    ]
+    
+    const router = createBrowserRouter(
+        createRoutesFromElements([
+            <Route element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                    <NavBar items={navItems} />
+                    <ErrorBoundary FallbackComponent={ErrorFallback}>
+                        <Outlet />
+                    </ErrorBoundary>
+                </ErrorBoundary>
+            }>
+                <Route path="/" element={<h1>{<Home />}</h1>} />
+                <Route path="/competitions" element={<Competitions />} />
+                <Route path="/results" element={<Results />} />
+                <Route path="/account" element={
+                    <ProtectedRoute requiredRole={Role.UNCONFIRMED_USER} redirectPath="/">
+                        <Account />
+                    </ProtectedRoute>}
+                />
+                <Route path="/admin/competitions" element={
+                    <ProtectedRoute requiredRole={Role.ADMIN} redirectPath="/">
+                        <AdminCompetitions />
+                    </ProtectedRoute>}
+                />
+                <Route path="/admin/competitions/:eid/*" element={
+                    <ProtectedRoute requiredRole={Role.ADMIN} redirectPath="/">
+                        <AdminCompetition />
+                    </ProtectedRoute>}
+                />
+                <Route path="/superadmin/*" element={
+                    <ProtectedRoute requiredRole={Role.SUPERADMIN} redirectPath="/">
+                        <SuperAdmin />
+                    </ProtectedRoute>}
+                />
+                <Route path="*" element={<h1>Not Found</h1>} />
+            </Route>
+        ])
+    )
 
-  const router = createBrowserRouter(
-    createRoutesFromElements([
-      <Route element={
+    useAutoLogin()
+
+    return (
         <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <NavBar items={navItems} />
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <Outlet />
-          </ErrorBoundary>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={lightTheme}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+                            <RouterProvider router={router} />
+                        </Box>
+                    </LocalizationProvider>
+                </ThemeProvider>
+            </QueryClientProvider>
         </ErrorBoundary>
-      }>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/competitions" element={<Competitions />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/admin/competitions" element={
-          <ProtectedRoute requiredRole={Role.ADMIN} redirectPath="/">
-            <AdminCompetitions />
-          </ProtectedRoute>}
-        />
-        <Route path="/admin/competitions/:eid/*" element={
-          <ProtectedRoute requiredRole={Role.ADMIN} redirectPath="/">
-            <AdminCompetition />
-          </ProtectedRoute>}
-        />
-        <Route path="/superadmin/*" element={
-          <ProtectedRoute requiredRole={Role.SUPERADMIN} redirectPath="/">
-            <SuperAdmin />
-          </ProtectedRoute>}
-        />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Route>
-    ])
-  )
-
-  useAutoLogin()
-
-  return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={lightTheme}>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-              <RouterProvider router={router} />
-            </Box>
-          </LocalizationProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </ErrorBoundary>
-  )
+    )
 }
 
 export default App
