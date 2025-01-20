ae3d4c5c978de649253fb343c423e6622d77f62f
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-20 23:35:16 +0100

    feat(GlobalsStates): add inscriptionDataAtom to manage athlete data in inscriptions

--- apps/competition-manager-web/src/GlobalsStates/index.ts
+++ apps/competition-manager-web/src/GlobalsStates/index.ts
@@ -1,7 +1,13 @@
-import { Competition, Inscription, TokenData } from '@competition-manager/schemas';
+import { Athlete, Competition, Inscription, TokenData } from '@competition-manager/schemas';
 import { atom } from 'jotai';
 
 export const userTokenAtom = atom<TokenData | 'NOT_LOGGED' | null>(null);
 
 export const competitionAtom = atom<Competition | null>(null);
 export const inscriptionsAtom = atom<Inscription[] | null>(null);
+
+type InscriptionData = {
+    athlete?: Athlete;
+};
+
+export const inscriptionDataAtom = atom<InscriptionData>({});
