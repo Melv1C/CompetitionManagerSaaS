feb2e86952e2eb85ebcaf8f5caa42a8a8eaf6651
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-21 12:29:51 +0100

    feat(utils): add getRemainingPlaces utility to calculate remaining event spots

---
+++ shared-packages/utils/src/getRemaningPlaces.ts
@@ -0,0 +1,13 @@
+import { CompetitionEvent, Inscription } from "@competition-manager/schemas"
+
+/**
+ * Get the remaining places of the event
+ * @param event The event to get the remaining places
+ * @param inscriptions The inscriptions of the competition
+ * @returns The remaining places
+ */
+export const getRemainingPlaces = (event: CompetitionEvent, inscriptions: Inscription[]): number => {
+    if (!event.place) throw new Error('Event has no place limit')
+    const eventInscriptions = inscriptions.filter((inscription) => inscription.competitionEvent.id === event.id)
+    return event.place - eventInscriptions.length
+}
--- shared-packages/utils/src/index.ts
+++ shared-packages/utils/src/index.ts
@@ -2,5 +2,6 @@ export * from "./isBestResult";
 export * from "./isAuthorize";
 export * from "./isNodeEnv";
 export * from "./getCategoryAbbr";
+export * from "./getRemaningPlaces";
 
 export * from "./checkInscription";
