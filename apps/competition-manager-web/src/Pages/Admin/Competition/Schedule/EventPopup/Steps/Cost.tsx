a8d8854b989b906a06f33107374514c54237a9f9
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-27 22:12:00 +0100

    feat(cost): enhance cost input with validation and navigation buttons

--- apps/competition-manager-web/src/Pages/Admin/Competition/Schedule/EventPopup/Steps/Cost.tsx
+++ apps/competition-manager-web/src/Pages/Admin/Competition/Schedule/EventPopup/Steps/Cost.tsx
@@ -1,5 +1,46 @@
-export const Cost = () => {
+import { Box } from "@mui/material"
+import { useTranslation } from "react-i18next"
+import { BaseFieldWith$, StepperButtons } from "../../../../../../Components"
+import { CompetitionEvent, CompetitionEvent$ } from "@competition-manager/schemas"
+import { useState } from "react"
+
+type CostProps = {
+    handleNext: () => void
+    handleBack: () => void
+    cost: CompetitionEvent["cost"]
+    setCost: (cost: CompetitionEvent["cost"]) => void
+}
+
+
+export const Cost: React.FC<CostProps> = ({
+    handleNext,
+    handleBack,
+    cost,
+    setCost
+}) => {
+
+    const { t } = useTranslation()
+
+    const [isValid, setIsValid] = useState(true)
+
     return (
-        <div>Cost</div>
+        <Box>
+
+            <BaseFieldWith$
+                id="cost"
+                type="number"
+                label={{ value: t('labels:price') }}
+                value={{ value: cost, onChange: setCost }}
+                validator={{ Schema$: CompetitionEvent$.shape.cost, isValid, setIsValid }}
+                formControlProps={{ fullWidth: true }}
+            />
+
+            <StepperButtons
+                buttons={[
+                    { label: t('buttons:previous'), onClick: handleBack, variant: 'outlined' },
+                    { label: t('buttons:next'), onClick: handleNext, disabled: !isValid }
+                ]}
+            />
+        </Box>
     )
 }
