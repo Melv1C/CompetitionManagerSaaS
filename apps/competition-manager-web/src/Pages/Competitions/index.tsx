b203edbeffc0a9546a8ea5e4c24df15705a2fdbf
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-27 16:02:24 +0100

    feat(competitions): add filtering for past competitions and update UI text

--- apps/competition-manager-web/src/Pages/Competitions/index.tsx
+++ apps/competition-manager-web/src/Pages/Competitions/index.tsx
@@ -2,18 +2,39 @@ import { useQuery } from "react-query";
 import { getCompetitions } from '../../api';
 import { ListCompetitions, Loading } from '../../Components';
 import { MaxWidth } from '../../Components/MaxWidth';
+import { useSearchParams } from "react-router-dom";
+import { Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
 
 export const Competitions: React.FC = () => {
-    const { data: competitions, isLoading, isError } = useQuery('competitions', () => getCompetitions({ from: new Date() }));
+
+    const { t } = useTranslation();
+
+    const [searchParams] = useSearchParams();
+    const isPast = searchParams.get('isPast') === 'true';
+
+    const { data: competitions, isLoading, isError } = useQuery(
+        ['competitions', isPast], 
+        () => getCompetitions({ from: isPast ? undefined : today, to: isPast ? tomorrow : undefined })
+    );
 
     if (isLoading) return <Loading />;
     if (isError) throw new Error('Error fetching competitions');
     if (!competitions) throw new Error('Unexpected error: competitions is undefined');
 
+    
+
     return (
         <MaxWidth>
-            <ListCompetitions competitions={competitions} isPast={false} />
+            <Typography variant="h4" align="center" gutterBottom>
+                {isPast ? t('glossary:results') : t('navigation:calendar')}
+            </Typography>
+            <ListCompetitions competitions={competitions} isPast={isPast} />
         </MaxWidth>
     );
 };
