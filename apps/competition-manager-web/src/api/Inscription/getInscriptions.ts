93dba50fb72f18281d57b42fdc8142dc3f43717f
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-28 17:13:04 +0100

    feat(inscription): update getInscriptions to use DisplayInscription schema and fetch data from API

--- apps/competition-manager-web/src/api/Inscription/getInscriptions.ts
+++ apps/competition-manager-web/src/api/Inscription/getInscriptions.ts
@@ -1,10 +1,8 @@
 
-import { Eid, Inscription$ } from "@competition-manager/schemas";
-//import { api } from "../../utils/api";
+import { DisplayInscription$, Eid } from "@competition-manager/schemas";
+import { api } from "../../utils/api";
 
 export const getInscriptions = async (competitionEid: Eid) => {
-    console.log(competitionEid);
-    return Inscription$.array().parse([]);
-    // const { data } = await api.get(`/competitions/${competitionEid}/inscriptions`);
-    // return Inscription$.array().parse(data);
+    const { data } = await api.get(`/competitions/${competitionEid}/inscriptions`);
+    return DisplayInscription$.array().parse(data);
 }
