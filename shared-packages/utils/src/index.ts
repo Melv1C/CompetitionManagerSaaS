a2db3f23a8bed911d9f8b0edda656b6d2ffb96da
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-30 19:46:39 +0100

    feat(fees): add getFees utility function to calculate fees based on total amount

--- shared-packages/utils/src/index.ts
+++ shared-packages/utils/src/index.ts
@@ -2,5 +2,6 @@ export * from "./isBestResult";
 export * from "./isAuthorize";
 export * from "./getCategoryAbbr";
 export * from "./getRemaningPlaces";
+export * from "./getFees";
 
 export * from "./checkInscription";
