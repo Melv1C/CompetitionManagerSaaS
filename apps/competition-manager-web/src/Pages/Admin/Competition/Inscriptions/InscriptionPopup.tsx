995ed9dad4cb68000ee1b76111713877ce6f13d3
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-30 17:16:43 +0100

    feat(inscription): add InscriptionPopup component for managing athlete inscriptions in a dialog

---
+++ apps/competition-manager-web/src/Pages/Admin/Competition/Inscriptions/InscriptionPopup.tsx
@@ -0,0 +1,28 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { CloseButton, InscriptionWizard, MaxWidth } from "../../../../Components";
+
+type InscriptionPopupProps = {
+    isVisible: boolean;
+    onClose: () => void;
+}
+
+export const InscriptionPopup: React.FC<InscriptionPopupProps> = ({ isVisible, onClose }) => {
+    return (
+        <Dialog
+            open={isVisible}
+            onClose={onClose} 
+            maxWidth="md"
+            fullWidth 
+            fullScreen 
+        >
+            <MaxWidth>
+                <DialogTitle>
+                    <CloseButton onClose={onClose} />
+                </DialogTitle>
+                <DialogContent>
+                    <InscriptionWizard isAdmin={true} />
+                </DialogContent>
+            </MaxWidth>
+        </Dialog>
+    )
+}
