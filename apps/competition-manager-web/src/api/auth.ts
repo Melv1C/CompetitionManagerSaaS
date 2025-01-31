75d4d05f5f1204be4cc649c61d42362d40e860e3
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-31 13:04:19 +0100

    feat(auth): update login and register functions to use Email and Password schemas; add resetPassword function

--- apps/competition-manager-web/src/api/auth.ts
+++ apps/competition-manager-web/src/api/auth.ts
@@ -1,14 +1,14 @@
-import { EncodeToken$ } from '@competition-manager/schemas';
+import { Email, EncodeToken$, Password } from '@competition-manager/schemas';
 import { api, apiWithCredentials, setAccessToken } from '../utils/api';
 
-export const login = async (email: string, password: string) => {
+export const login = async (email: Email, password: Password) => {
     const { data } = await api.post('/users/login', { email, password });
     const accessToken = EncodeToken$.parse(data);
     setAccessToken(accessToken);
     return accessToken;
 };
 
-export const register = async (email: string, password: string) => {
+export const register = async (email: Email, password: Password) => {
     const { data } = await api.post('/users/register', { email, password });
     const accessToken = EncodeToken$.parse(data);
     setAccessToken(accessToken);
@@ -25,5 +25,10 @@ export const getRefreshToken = async () => {
     setAccessToken(accessToken);
     return accessToken;
 };
+
+export const resetPassword = async (token: string, newPassword: Password) => {
+    const { data } = await api.post('/users/reset-password', { newPassword }, { params: { token } });
+    return data;
+}
 
 
