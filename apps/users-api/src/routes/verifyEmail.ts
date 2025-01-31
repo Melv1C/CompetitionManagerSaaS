82be061b4b5e78d3eaea225c731e28bd7bd523fe
Melv1C <melvyn.claes@student.uclouvain.be>
2025-01-31 12:13:36 +0100

    fix(verify-email): update redirect URL to use BASE_URL and redirect to the account page

--- apps/users-api/src/routes/verifyEmail.ts
+++ apps/users-api/src/routes/verifyEmail.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { z } from 'zod';
 import { prisma } from '@competition-manager/prisma';
-import { parseRequest, Key, verifyVerificationToken, generateAccessToken } from '@competition-manager/backend-utils';
-import { EncodeToken$, Role, User$ } from '@competition-manager/schemas';
-import { UserToTokenData } from '../utils';
+import { parseRequest, Key, verifyVerificationToken } from '@competition-manager/backend-utils';
+import { EncodeToken$, Role } from '@competition-manager/schemas';
+import { env } from '..';
 
 export const router = Router();
 
@@ -35,7 +35,7 @@ router.get(
                 res.status(400).send('User already verified');
                 return;
             }
-            const updatetdUser = await prisma.user.update({
+            await prisma.user.update({
                 where: {
                     id: user.id
                 },
@@ -46,9 +46,8 @@ router.get(
                     preferences: true
                 }
             });
-            const newTokenData = UserToTokenData(User$.parse(updatetdUser));
-            const accessToken = generateAccessToken(newTokenData);
-            res.redirect(`${process.env.FRONTEND_URL}/token?accessToken=${accessToken}`);
+            // Redirect to account page
+            res.redirect(`${env.BASE_URL}/account`);
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Internal server error' });
