c5e2e3a48acffab444fd44a2f17c111a93c04144
Melv1C <melvyn.claes@student.uclouvain.be>
2025-02-04 16:05:13 +0100

    feat(home): improve responsiveness of Home page layout for mobile devices

--- apps/competition-manager-web/src/Pages/Home/index.tsx
+++ apps/competition-manager-web/src/Pages/Home/index.tsx
@@ -2,24 +2,27 @@ import { useTranslation } from "react-i18next";
 import { Logo, MaxWidth } from "../../Components";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useDeviceSize } from "../../hooks";
 
 export const Home = () => {
     const { t } = useTranslation('home');
     const navigate = useNavigate();
 
+    const {isMobile} = useDeviceSize();
+
     return (
         <MaxWidth>
             <Box sx={{ py: 6 }}>
-                <Stack direction={{ xs: "column", md: "row" }} alignItems="center" spacing={4}>
+                <Stack direction={isMobile ? "column" : "row"} spacing={isMobile ? 4 : 8} alignItems="center">
                     {/* Texte à gauche */}
-                    <Box flex={1} textAlign={{ xs: "center", md: "left" }}>
+                    <Box flex={1} textAlign={isMobile ? "center" : "left"}>
                         <Typography variant="h3" fontWeight={700} gutterBottom>
                             {t("title")}
                         </Typography>
                         <Typography variant="h6" color="text.secondary" gutterBottom>
                             {t("description")}
                         </Typography>
-                        <Stack direction="row" spacing={2} justifyContent={{ xs: "center", md: "flex-start" }}>
+                        <Stack direction="row" spacing={2} justifyContent={isMobile ? "center" : "flex-start"}>
                             <Button variant="contained" color="primary" onClick={() => navigate('/competitions')}>
                                 {t("viewCalendar")}
                             </Button>
@@ -30,9 +33,11 @@ export const Home = () => {
                     </Box>
 
                     {/* Logo à droite */}
-                    <Box flex={1} display="flex" justifyContent="center">
-                        <Logo color="black" sx={{ height: '20rem' }} />
-                    </Box>
+                    {!isMobile && (
+                        <Box flex={1} display="flex" justifyContent="center">
+                            <Logo color="black" sx={{ width: "100%", maxWidth: 400 }} />
+                        </Box>
+                    )}
                 </Stack>
             </Box>
         </MaxWidth>
