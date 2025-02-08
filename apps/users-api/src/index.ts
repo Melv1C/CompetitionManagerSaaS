346d85c79bc142767320ced7deb8c879583b6b49
Riwan <riwan.claes@student.uclouvain.be>
2025-02-08 11:23:03 +0100

    feat: add i18next for internationalization support and create translation files

--- apps/users-api/src/index.ts
+++ apps/users-api/src/index.ts
@@ -5,12 +5,24 @@ import cookieParser from 'cookie-parser';
 import { corsMiddleware, Key, logRequestMiddleware, OmitType } from '@competition-manager/backend-utils';
 import { logger } from "./logger";
 import { env } from "./env";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import middleware from "i18next-http-middleware";
+
+i18next.use(Backend).use(middleware.LanguageDetector).init({
+    backend: {
+        loadPath: `src/translations/{{lng}}.json`,
+    },
+    fallbackLng: 'en'
+});
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(corsMiddleware);
+
+app.use(middleware.handle(i18next));
 
 const omit: OmitType = [
     { key: Key.Body, field: 'password' },
