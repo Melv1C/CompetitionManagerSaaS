d0e3fd38b8e2ff25bc35b95d246139c205ec85c0
Riwan <riwan.claes@student.uclouvain.be>
2025-02-08 13:21:51 +0100

    feat(results): add Result and ResultDetail models with migration and schema updates

--- shared-packages/schemas/src/Result.ts
+++ shared-packages/schemas/src/Result.ts
@@ -4,37 +4,42 @@ import { Bib$, Boolean$, Eid$, Id$ } from './Base';
 import { CompetitionEvent$ } from './CompetitionEvent';
 import { Club$ } from './Club';
 
+export enum AttemptValue {
+    X = 'X',
+    O = 'O',
+    PASS = '-',
+}
+
+
 export const ResultDetails$ = z.object({
     id: Id$,
-    //eid: Eid$, pas utile je pense car on n'aura pas te route pour les détails seulement
-    trynum: z.number(),
-    isBest: Boolean$.default(false),
-isOfficialBest: Boolean$.default(false),
+    tryNumber: z.number(),
     value: z.number(),
-    result: z.string(),
-    wind: z.string().nullish(), // or string ?
+    attempts: z.nativeEnum(AttemptValue).array().max(3).min(1).nullish(),
+    wind: z.number().nullish(),
+    isBest: Boolean$.default(false),
+    isOfficialBest: Boolean$.default(false),
 });
 export type ResultDetails = z.infer<typeof ResultDetails$>;
 
 export const Result$ = z.object({
     id: Id$,
     eid: Eid$,
-    //competitionId: Id$, pas nécessaire je pense mais il faut la relation en db
     competitionEvent: CompetitionEvent$,
+    //inscription: Inscription$,
     athlete: Athlete$,
     bib: Bib$,
     club: Club$,
 
-    heat: z.number(),
+    heat: z.number().default(1),
     initialOrder: z.number(),
     tempOrder: z.number(),
-    finalOrder: z.number(),
+    finalOrder: z.number().nullish(),
 
-    value: z.number(),
-    result: z.string(),
-    wind: z.string().nullish(), // or string ?
-    points: z.number().nullish(),
+    value: z.number().nullish(),
+    wind: z.number().nullish(),
+    points: z.number().int().nonnegative().nullish(),
 
-    details: ResultDetails$.array(),
+    details: ResultDetails$.array().default([]),
 });
 export type Result = z.infer<typeof Result$>;
