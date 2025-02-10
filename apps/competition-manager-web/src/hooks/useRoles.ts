1566d598b298246c58c00dfd151d8340ab2495ed
Melv1C <melvyn.claes@student.uclouvain.be>
2025-02-10 11:01:24 +0100

    feat(hooks): enhance role validation with authorization checks

--- apps/competition-manager-web/src/hooks/useRoles.ts
+++ apps/competition-manager-web/src/hooks/useRoles.ts
@@ -1,23 +1,27 @@
 import { Role } from "@competition-manager/schemas";
 import { useAtomValue } from "jotai";
 import { userTokenAtom } from "../GlobalsStates";
+import { isAuthorized } from "@competition-manager/utils";
 
 export const useRoles = () => {
     const userToken = useAtomValue(userTokenAtom);
 
     const isNotLogged = userToken === 'NOT_LOGGED';
 
-    const isLogged = userToken !== null && userToken !== 'NOT_LOGGED';
+    const isLogged = userToken !== null && userToken !== 'NOT_LOGGED' && isAuthorized(userToken, Role.UNCONFIRMED_USER);
 
-    const isSuperAdmin = isLogged && userToken.role === Role.SUPERADMIN;
+    const isUser = userToken !== null && userToken !== 'NOT_LOGGED' && isAuthorized(userToken, Role.USER);
 
-    const isClub = isSuperAdmin || (isLogged && userToken.role === Role.CLUB);
+    const isAdmin = userToken !== null && userToken !== 'NOT_LOGGED' && isAuthorized(userToken, Role.ADMIN);
 
-    const isAdmin = isClub || (isLogged && userToken.role === Role.ADMIN);
+    const isClub = userToken !== null && userToken !== 'NOT_LOGGED' && isAuthorized(userToken, Role.CLUB);
+
+    const isSuperAdmin = userToken !== null && userToken !== 'NOT_LOGGED' && isAuthorized(userToken, Role.SUPERADMIN);
 
     return {
         isNotLogged,
         isLogged,
+        isUser,
         isSuperAdmin,
         isClub,
         isAdmin
