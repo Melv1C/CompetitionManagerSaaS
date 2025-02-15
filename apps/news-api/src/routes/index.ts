41491b9609d29ad9a74ae6be9507c64988043b4a
Melv1C <melvyn.claes@student.uclouvain.be>
2025-02-15 19:03:14 +0100

    refactor(news-api): remove unused files and configurations

--- apps/news-api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-// Import all routes here
-import { router as getAllNewsRouter } from './getAllNews';
-import { router as createNewsRouter } from './createNews';
-
-export default [
-    getAllNewsRouter,
-    createNewsRouter
-];
