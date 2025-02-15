65512af4b4b3c5ae655a19054823d82797df6f93
Melv1C <melvyn.claes@student.uclouvain.be>
2025-02-15 19:04:50 +0100

    refactor(news): remove News in superadmin

--- apps/competition-manager-web/src/Pages/SuperAdmin/index.tsx
+++ apps/competition-manager-web/src/Pages/SuperAdmin/index.tsx
@@ -2,16 +2,14 @@
 
 import { Box, Typography } from "@mui/material"
 import { SideNav } from "../../Components/SideNav";
-import { faList, faNewspaper, faReceipt } from "@fortawesome/free-solid-svg-icons";
+import { faList, faReceipt } from "@fortawesome/free-solid-svg-icons";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Offers } from "./Offers";
 import { CLOSED_SIDENAV_WIDTH, OPEN_SIDENAV_WIDTH } from "../../utils/constants";
 import { useState } from "react";
 import { Logs } from "./Logs";
-import { News } from "./News";
 
 const MenuItems = [
-    { text: 'News', icon: faNewspaper, link: '/superadmin/news' },
     { text: 'Logs', icon: faList, link: '/superadmin/logs' },
     { text: 'Offers', icon: faReceipt, link: '/superadmin/offers' },
 ]
@@ -36,7 +34,6 @@ export const SuperAdmin = () => {
             >
                 <Routes>
                     <Route path="/" element={<Typography variant="h4">SuperAdmin</Typography>} />
-                    <Route path="/news" element={<News />} />
                     <Route path="/logs" element={<Logs />} />
                     <Route path="/offers" element={<Offers />} />
                     <Route path="*" element={<Navigate to="/" />} />
