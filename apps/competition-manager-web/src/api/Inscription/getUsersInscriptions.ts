fe76481409bb2a0b246b8989d100ca7416b73fcd
Melv1C <melvyn.claes@student.uclouvain.be>
2025-02-22 15:27:45 +0100

    fix(inscriptions): handle local environment by returning empty inscriptions list

--- apps/competition-manager-web/src/api/Inscription/getUsersInscriptions.ts
+++ apps/competition-manager-web/src/api/Inscription/getUsersInscriptions.ts
@@ -1,9 +1,13 @@
-import { Eid, Inscription$ } from "@competition-manager/schemas";
+import { Eid, Inscription$, NODE_ENV } from "@competition-manager/schemas";
 import { api } from "../../utils/api";
+import { isNodeEnv } from "../../env";
 
 
 export const getUsersInscriptions = async (competitionEid?: Eid) => {
     try {
+        if (isNodeEnv(NODE_ENV.LOCAL)) {
+            return [];
+        }
         const { data } = await api.get('/users/me/inscriptions', {
             params: {
                 competitionEid
--- apps/competition-manager-web/src/api/Inscription/index.ts
+++ apps/competition-manager-web/src/api/Inscription/index.ts
@@ -2,4 +2,4 @@ export * from "./getInscriptions";
 export * from "./getUsersInscriptions";
 export * from "./createInscriptions";
 export * from "./getAdminInscriptions";
-export * from "./updateInscriptions";
+export * from "./updateInscriptions";  
